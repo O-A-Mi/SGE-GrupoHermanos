7a7e12a0fd04a8d93d2d3435d1eb9853dd4f504d
O-A-Mi <o.ou.a.mis@gmail.com>
2025-08-14 10:01:59 -0300

    fix

--- src/components/Navigator/index.jsx
+++ src/components/Navigator/index.jsx
@@ -23,7 +23,7 @@ const Navigator = () => {
                 </div>
 
                 <div>
-                    <i class="fa-solid fa-bars"></i>
+                    <i className="fa-solid fa-bars"></i>
                 </div>
             </div>
         </>
